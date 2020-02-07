2a6423c253eea1e3d435bb79a0233ae367183fff
mark <fx355@nyu.edu>
2020-02-07 15:15:26 -0500

    sqlite

--- test.py
+++ test.py
@@ -2,8 +2,20 @@ import sqlite3 as lite
 
 from wig import WIG
 
+datafile = 'headlines_reduce.tsv'
+
+
+def read():
+    conn = lite.connect(datafile)
+    c = conn.cursor()
+    result = c.execute('select date, title from headliens_reduce;').fetchall()
+    conn.close()
+    return result
+
 
 def main():
+    data = read()
+    wig = WIG()
     pass
 
 
